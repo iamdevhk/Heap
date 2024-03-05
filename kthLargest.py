0938e78413dae2b0644959f3229d4b84ea73d57c
HARIKRISHNAN RK <harikrishnan8888888@gmail.com>
2024-03-04 16:41:44 -0800

    added code for leetcode 703. Kth Largest Element in a Stream

---
+++ kthLargest.py
@@ -0,0 +1,15 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k=k
+        self.data=nums
+        heapq.heapify(self.data)
+        while len(self.data)>k:
+            heapq.heappop(self.data)
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.data,val)
+        if len(self.data)>self.k:
+            heapq.heappop(self.data)
+        return self.data[0]
